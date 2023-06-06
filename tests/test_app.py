f054f61aee86af368dec1f0b2dcecfa6cac0af15
Minh <minhpc@ikameglobal.com>
2023-06-06 12:50:14 +0700

    initial commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,13 +9,13 @@ import pytest
 from src import SampleClass
 
 
-@pytest.fixture
-def api_key():
-    api_key = os.environ.get("API_KEY", None)
-    assert api_key is not None
-    return api_key
-
-
-def test_report(api_key):
-    report = SampleClass(api_key=api_key)
-    assert report is not None
+# @pytest.fixture
+# def api_key():
+#     api_key = os.environ.get("API_KEY", None)
+#     assert api_key is not None
+#     return api_key
+#
+#
+# def test_report(api_key):
+#     report = SampleClass(api_key=api_key)
+#     assert report is not None
